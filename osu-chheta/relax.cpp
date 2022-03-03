158fc5326945933f0633c5ecd2a48e7194d0017c
0verjoY <overjoy.psm@gmail.com>
2022-03-03 17:07:34 +0100

    Bladie

--- osu-chheta/relax.cpp
+++ osu-chheta/relax.cpp
@@ -1,8 +1,8 @@
 #include "relax.hpp"
 #include "signature.h"
 
-const char* sig = TIME_SIGNATURE->getPattern().c_str();
-const char* mask = "xxxxxxx";
+//const char* sig = TIME_SIGNATURE->getPattern().c_str();
+//const char* mask = "xxxxxxx";
 
 template <typename T> T readMemory(char* section, HANDLE process)
 {
@@ -24,10 +24,17 @@ void startRelax(Beatmap map, HANDLE process)
     auto action = actions.begin();
     int time = -1;
 
-    uint32_t timeSignature = (uint32_t)scanExtern(const_cast<char*&>(sig), const_cast<char*&>(mask), &process);
-    uint32_t timeAddress = readMemory<uint32_t>((char*)timeSignature + TIME_SIGNATURE->getOffset(), process);
+    char sig[7] = { 0x7E, 0x55, 0x8B, 0x76, 0x10, 0xDB, 0x05, };
+    char mask[7] = { 'x', 'x', 'x', 'x', 'x', 'x', 'x', };
 
-    while (!GetAsyncKeyState(VK_RETURN)) {
+    uint32_t timeSignature = (uint32_t)scanExtern(sig, mask, &process);
+    uint32_t timeAddress = readMemory<uint32_t>((char*)timeSignature + 0x07, process);
+
+    std::cout << std::hex << "Time address: " << timeAddress << std::endl;
+
+
+    while (!GetAsyncKeyState(VK_RETURN)) 
+    {
         Sleep(100);
     }
 
